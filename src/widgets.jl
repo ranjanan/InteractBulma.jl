617ecb97c23c14a167a7e4dc174b7bd816fabc25
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-16 13:47:35 +0100

    adjust to className

--- src/widgets.jl
+++ src/widgets.jl
@@ -1,5 +1,5 @@
-function radio(T::Bulma, s, key, val, vmodel; class = "", kwargs...)
+function radio(T::Bulma, s, key, val, vmodel; class = nothing, className = InteractBase._replace_className(class), kwargs...)
     id = string(gensym())
-    (Node(:input, className = "is-checkradio $class", attributes = Dict(:id => id, :name=>s, :type=>"radio", vmodel=>"value", :value=>val)),
+    (Node(:input, className = "is-checkradio $className", attributes = Dict(:id => id, :name=>s, :type=>"radio", vmodel=>"value", :value=>val)),
         Node(:label, key, className = "form-radio", attributes = merge(Dict(:for => id), Dict(kwargs))))
 end
